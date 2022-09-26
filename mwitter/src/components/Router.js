dad094ebaec0d058c85a21da571e3e692c26811a
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-09-26 18:00:38 +0900

    Login form

---
+++ mwitter/src/components/Router.js
@@ -0,0 +1,26 @@
+import React, { useCallback, useState } from "react";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = ({isLoggedIn}) => {
+    return (
+        <Router>
+            <Switch>
+                {isLoggedIn ? (
+                <>
+                <Route>
+                    <Home />
+                </Route>
+                </> 
+                ) : (
+                <Route>
+                    <Auth />
+                </Route>
+                )}
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRouter;
